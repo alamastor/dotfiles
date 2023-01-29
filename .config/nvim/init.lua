c2d560c8c340ca50df2adcb4dd570565d9d609a7
Alistair McKelvie <amckelvie@seek.com.au>
2023-01-29 20:12:18 +1100

    Add nvim config files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+require "plugins"
+require "treesitter"
+
+-- Nvim Tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+require("nvim-tree").setup()
+
+-- Gruvbox Theme
+vim.o.background = "dark"
+require("gruvbox").setup({
+  contrast = "hard"
+})
+vim.cmd([[colorscheme gruvbox]])
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,48 @@
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+  git = {
+    clone_timeout = 300, -- Timeout, in seconds, for git clones
+  },
+}
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Nvim Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Nvim Tree
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+  -- Gruvbox
+  use { "ellisonleao/gruvbox.nvim" }
+end)
