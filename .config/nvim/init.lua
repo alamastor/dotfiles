591258ad1524f1ab27cca498a03617aa7af3effa
Alistair McKelvie <amckelvie@seek.com.au>
2023-01-29 20:59:13 +1100

    Set options in nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,26 @@ require "user/packer"
 require "user/treesitter"
 require "user/nvim-tree"
 require "user/theme"
+
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus" -- access sytem clipboard
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.fileencoding = "utf-8"
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.mouse = "a" -- enable mouse
+vim.opt.showtabline = 0 -- always show tabs
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.updatetime = 300 -- faster completion (4000ms default)
+vim.opt.writebackup = false -- lock in use files
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.guifont = "monospace:h17"
