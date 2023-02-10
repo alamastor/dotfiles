f014a6deb43a84120d88d2c2d6703c1c301c4467
Alistair McKelvie <amckelvie@seek.com.au>
2023-02-10 14:21:32 +1100

    Add null-ls nvim config

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -24,4 +24,10 @@ return require('packer').startup(function(use)
   use "neovim/nvim-lspconfig"
   use "williamboman/mason.nvim"
   use "williamboman/mason-lspconfig.nvim"
+  use {
+    "jose-elias-alvarez/null-ls.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim"
+    }
+  }
 end)
