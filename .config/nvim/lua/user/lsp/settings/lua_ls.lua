fc37fe881aa288160b130d8e7c3552d4727d8763
Alistair McKelvie <amckelvie@seek.com.au>
2023-03-12 09:20:43 +1100

    Stop Lua 3rd party lib install popups

--- .config/nvim/lua/user/lsp/settings/lua_ls.lua
+++ .config/nvim/lua/user/lsp/settings/lua_ls.lua
@@ -12,6 +12,8 @@ return {
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
+        -- Stop popups trying to install 3rd party libs
+        checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
