643d44307010de6b5e8de665efe75a9041276973
Alistair McKelvie <alistair.mckelvie@gmail.com>
2023-04-15 13:19:30 +1000

    Fix keybinds for Nvim rust-tools

--- .config/nvim/lua/user/rust-tools.lua
+++ .config/nvim/lua/user/rust-tools.lua
@@ -1,15 +1,11 @@
 local mason = require"user.mason"
+local handlers = require"user.lsp.handlers"
 mason.install_package("codelldb")
 local rt = require"rust-tools"
 
 rt.setup({
   server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
+    on_attach = handlers.on_attach,
   },
   dap = {
     adapter = require"rust-tools.dap".get_codelldb_adapter(
