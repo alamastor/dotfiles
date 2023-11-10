d8102603149d0876d03f61237af27ef44dd26a45
Alistair McKelvie <amckelvie@seek.com.au>
2023-11-10 17:09:17 +1100

    Require active selection of option to accept in Nvim cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -66,9 +66,8 @@ return {
           i = cmp.mapping.abort(),
           c = cmp.mapping.close(),
         }),
-        -- Accept currently selected item. If none selected, `select` first item.
         -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() and has_words_before() then
             cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
