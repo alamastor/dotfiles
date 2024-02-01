624af6892d5cf3a6908acea6be1f8a331a0d0839
Alistair McKelvie <amckelvie@seek.com.au>
2024-02-01 14:14:20 +1100

    Make Nvim Telescope search hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,18 @@ return {
   "nvim-telescope/telescope.nvim",
   config = function()
     require("telescope").setup({
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--hidden",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+        },
+      },
       extensions = {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown({}),
