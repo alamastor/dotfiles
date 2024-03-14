b7e0e5253ec1fa8d0c05e6ebbdbde7058edff491
Alistair McKelvie <amckelvie@seek.com.au>
2024-03-14 16:04:29 +1100

    Nvim; use tabs for groovy files

---
+++ .config/nvim/ftplugin/groovy.lua
@@ -0,0 +1,10 @@
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt_local.listchars = {
+  tab = "  ",
+  trail = "·",
+  extends = "",
+  precedes = "",
+  lead = ".",
+}
