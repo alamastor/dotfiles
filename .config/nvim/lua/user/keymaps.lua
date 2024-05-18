d6254764039cc02610c95d1f644413222af33c30
Alistair McKelvie <alistair.mckelvie@gmail.com>
2024-05-18 12:33:32 +1000

    Redo keymaps with which-key

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,6 +1,5 @@
 local M = {}
-local keymap = vim.keymap.set
-local opts = { silent = true }
+local wk = require("which-key")
 local prequire = require("user.common").prequire
 
 local function remap(binding_fn_name)
@@ -24,7 +23,6 @@ end
 
 -- Remap space as leader key
 -- Needs to be done before using leader in any maps!
-keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
 -- Reload this file
@@ -32,51 +30,152 @@ M.reload = function()
   R("user.keymaps")
   print("Keymaps reloaded.")
 end
-keymap("n", "<leader>rr", "<cmd>lua require('user.keymaps').reload()<cr>")
+wk.register({
+  ["<leader>r"] = {
+    name = "+reload",
+    r = { "<cmd>lua require('user.keymaps').reload()<cr>", "Reload Keymaps" },
+  },
+})
 
 -- NvimTree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+wk.register({ ["<leader>e"] = { ":NvimTreeToggle<CR>", "Toggle Nvim Tree" } })
 
 -- DAP
-keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
-keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
-keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
-keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", opts)
-keymap("n", "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", opts)
-keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
-keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
-keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
-keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
-keymap("n", "<leader>dn", remap("test_method"), opts)
-keymap("v", "<leader>ds", "<cmd>lua require('dap-python').debug_selection()<cr>", opts)
+wk.register({
+  ["<leader>d"] = {
+    name = "+dap",
+    b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "DAP toggle breakpoint" },
+    c = { "<cmd>lua require'dap'.continue()<cr>", "DAP continue" },
+    i = { "<cmd>lua require'dap'.step_into()<cr>", "DAP step into" },
+    o = { "<cmd>lua require'dap'.step_over()<cr>", "DAP step over" },
+    O = { "<cmd>lua require'dap'.step_out()<cr>", "DAP step out" },
+    r = { "<cmd>lua require'dap'.repl.toggle()<cr>", "DAP toggle REPL" },
+    l = { "<cmd>lua require'dap'.run_last()<cr>", "DAP run last" },
+    u = { "<cmd>lua require'dapui'.toggle()<cr>", "DAP toggle UI" },
+    t = { "<cmd>lua require'dap'.terminate()<cr>", "DAP terminate" },
+    n = { remap("test_method"), "DAP test method" },
+  },
+})
 
-keymap("n", "<leader>lf", remap("format_file"), { noremap = true, silent = true })
-keymap("n", "gD", remap("go_to_declaration"), { noremap = true, silent = true })
-keymap("n", "gd", remap("go_to_definition"), { noremap = true, silent = true })
-keymap("n", "K", remap("documentation"), { noremap = true, silent = true })
-keymap("n", "gi", remap("implementation"), { noremap = true, silent = true })
-keymap("n", "gr", remap("references"), { noremap = true, silent = true })
-keymap("n", "gl", remap("diagnostic"), { noremap = true, silent = true })
-keymap("n", "<leader>la", remap("code_action"), { noremap = true, silent = true })
-keymap("n", "<leader>lj", remap("next_diagnostic"), { noremap = true, silent = true })
-keymap("n", "<leader>lk", remap("next_diagnostic"), { noremap = true, silent = true })
-keymap("n", "<leader>lr", remap("rename"), { noremap = true, silent = true })
-keymap("n", "<leader>ls", remap("signature_help"), { noremap = true, silent = true })
-keymap("n", "<leader>lo", remap("organize_imports"), { noremap = true, silent = true })
-keymap("n", "<leader>lev", remap("extract_variable"), { noremap = true, silent = true })
-keymap("n", "<leader>lec", remap("extract_constant"), { noremap = true, silent = true })
-keymap("n", "<leader>lem", remap("extract_method"), { noremap = true, silent = true })
+wk.register({ K = { remap("documentation"), "LSP docs" } })
 
-keymap("n", "<leader>li", "<cmd>LspInfo<cr>", { noremap = true, silent = true })
-keymap("n", "<leader>lI", "<cmd>Mason<cr>", { noremap = true, silent = true })
+wk.register({
+  g = {
+    name = "+goto",
+    D = { remap("go_to_declaration"), "Goto declaration" },
+    d = { remap("go_to_definition"), "Goto definition" },
+    i = { remap("implementation"), "Goto implementation" },
+    r = { remap("references"), "Goto references" },
+    l = { remap("diagnostic"), "Goto diagnostics" },
+  },
+})
+
+wk.register({
+  ["<leader>l"] = {
+    name = "+lsp",
+    f = { remap("format_file"), "LSP format file" },
+    a = { remap("code_action"), "LSP code action" },
+    j = { remap("prev_diagnostic"), "LSP prev diagnostic" },
+    k = { remap("next_diagnostic"), "LSP next diagnostic" },
+    r = { remap("rename"), "Rename" },
+    s = { remap("signature_help"), "LSP signature help" },
+    o = { remap("organize_imports"), "LSP organise imports" },
+    e = {
+      name = "+extract",
+      v = { remap("extract_variable"), "LSP extract variable" },
+      c = { remap("extract_constant"), "LSP extract constant" },
+      m = { remap("extract_method"), "LSP extract method" },
+    },
+    i = { "<cmd>LspInfo<cr>", "LSP info" },
+    I = { "<cmd>Mason<cr>", "Mason" },
+  },
+})
 
 -- Telescope
-keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true, silent = true })
-keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
-keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true, silent = true })
-keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true, silent = true })
-keymap("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { noremap = true, silent = true })
+wk.register({
+  ["<leader>f"] = {
+    name = "+telescope",
+    b = { "<cmd>Telescope buffers<cr>", "Telescope buffers" },
+    g = { "<cmd>Telescope live_grep<cr>", "Telescope grep" },
+    h = { "<cmd>Telescope help_tags<cr>", "Telescope help" },
+    f = { "<cmd>Telescope find_files<cr>", "Telescope files" },
+    d = { "<cmd>Telescope diagnostics<cr>", "Telescope diagnostics" },
+    r = { "<cmd>Telescope resume<cr>", "Telescope resume" },
+    j = { "<cmd>Telescope jumplist<cr>", "Telescope jumplist" },
+    t = { name = "+git", f = { "<cmd>Telescope git_files<cr>", "Telescope Git files" } },
+  },
+})
 
 -- Noice
-keymap("n", "<leader>c", "<cmd>Noice dismiss<cr>", { noremap = true, silent = true })
+wk.register({ ["<leader>c"] = { "<cmd>Noice dismiss<cr>", "Noice dismiss" } })
+
+-- Git
+local gs = require("gitsigns")
+wk.register({
+  ["<leader>h"] = {
+    name = "+git",
+    S = { ":Gitsigns stage_buffer<CR>", "Git stage buffer" },
+    u = { ":Gitsigns undo_stage_hunk<CR>", "Git undo stage hunk" },
+    R = { ":Gitsigns reset_buffer<CR>", "Git reset buffer" },
+    p = { ":Gitsigns preview_hunk<CR>", "Git preview hunk" },
+    b = {
+      function()
+        gs.blame_line({ full = true })
+      end,
+      "Git blame line",
+    },
+    d = { ":Gitsigns diffthis<CR>", "Git open diff" },
+    D = {
+      function()
+        gs.diffthis("~")
+      end,
+      "Git open dif with prev commit",
+    },
+    t = {
+      name = "Toggle",
+      b = { ":Gitsigns toggle_current_line_blame<CR>", "Git toggle blame" },
+      d = { ":Gitsigns toggle_deleted<CR>", "Git toggle deleted" },
+    },
+  },
+  s = { ":Gitsigns stage_hunk<CR>", "Git stage hunk", mode = { "v", "n" } },
+  r = { ":Gitsigns reset_hunk<CR>", "Git reset hunk", mode = { "v", "n" } },
+})
+
+-- Navigation
+wk.register({
+  ["]"] = {
+    name = "+next",
+    c = {
+      function()
+        if vim.wo.diff then
+          return "]c"
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return "<Ignore>"
+      end,
+      "Next Git hunk",
+    },
+  },
+})
+
+wk.register({
+  ["["] = {
+    name = "+prev",
+    c = {
+      function()
+        if vim.wo.diff then
+          return "[c"
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return "<Ignore>"
+      end,
+      "Prev Git hunk",
+    },
+  },
+})
+
 return M
