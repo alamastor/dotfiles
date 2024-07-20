5b546e437e09c519222f6d85e33673a7c70b75c2
Alistair McKelvie <alistair.mckelvie@gmail.com>
2024-07-20 21:21:16 +1000

    Update Nvim deps

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,33 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
-    signs = {
-      add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = {
-        hl = "GitSignsChange",
-        text = "▎",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-      delete = {
-        hl = "GitSignsDelete",
-        text = "契",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = "契",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = "▎",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
-    },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     watch_gitdir = {
       interval = 1000,
@@ -49,18 +22,6 @@ return {
       relative = "cursor",
       row = 0,
       col = 1,
-    },
-    on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
-
-      local function map(mode, l, r, opts)
-        opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
-      end
-    end,
-    yadm = {
-      enable = true,
     },
   },
 }
