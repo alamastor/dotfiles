0286539af655283f5a78924604e96df7c007461d
Alistair McKelvie <alistair.mckelvie@gmail.com>
2024-08-25 12:11:06 +1000

    Fix Wezterm on Linux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,4 +7,8 @@ config.color_scheme = "Rosé Pine (Gogh)"
 config.font = wezterm.font({ family = "FiraCode NFM" })
 config.font_size = 13.0
 
+config.enable_wayland = false
+
+config.audible_bell = "Disabled"
+
 return config
