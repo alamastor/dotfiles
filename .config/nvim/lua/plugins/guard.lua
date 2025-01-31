80b682b5500faf212beda4c2712099b96438c897
Alistair McKelvie <amckelvie@seek.com.au>
2025-01-31 15:27:03 +1100

    Fix Nvim Guard Ruff config

--- .config/nvim/lua/plugins/guard.lua
+++ .config/nvim/lua/plugins/guard.lua
@@ -8,8 +8,47 @@ return {
   },
   config = function()
     local ft = require("guard.filetype")
+    local lint = require("guard.lint")
 
-    ft("python"):fmt("ruff"):lint("ruff")
+    ft("python"):fmt("ruff"):lint({
+      -- Custom ruff config, builtin doesn't include `check` arg, which is now required
+      cmd = "ruff",
+      args = {
+        "check",
+        "-n",
+        "-e",
+        "--output-format",
+        "json",
+        "-",
+        "--stdin-filename",
+      },
+      stdin = true,
+      fname = true,
+      parse = lint.from_json({
+        attributes = {
+          severity = "type",
+          lnum = function(js)
+            return js["location"]["row"]
+          end,
+          col = function(js)
+            return js["location"]["column"]
+          end,
+        },
+        severities = {
+          E = lint.severities.error, -- pycodestyle errors
+          W = lint.severities.warning, -- pycodestyle warnings
+          F = lint.severities.info, -- pyflakes
+          A = lint.severities.info, -- flake8-builtins
+          B = lint.severities.warning, -- flake8-bugbear
+          C = lint.severities.warning, -- flake8-comprehensions
+          T = lint.severities.info, -- flake8-print
+          U = lint.severities.info, -- pyupgrade
+          D = lint.severities.info, -- pydocstyle
+          M = lint.severities.into, -- Meta
+        },
+        source = "ruff",
+      }),
+    })
     ft("lua"):fmt("stylua")
     ft("sh"):lint("shellcheck")
     ft("typescript"):fmt("prettier")
